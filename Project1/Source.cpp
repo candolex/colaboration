b1f4e170ec9f0d2e99d0a3a4fdd13fbd184ad2b7
DnlKvdn <dnl.kvdn@outlook.com>
2021-07-21 20:18:08 +0300

    CommitDnl

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,2 +1,40 @@
 #include <iostream>
+using namespace std;
+
+class TestDnl //Мой класс
+{
+private:
+	int data = 0;
+public:
+	TestDnl()
+	{
+
+	}
+	void cinData()
+	{
+		cout << "Add Test data:\t";
+		cin >> data;
+	}
+	void printData()
+	{
+		cout << "Your test data:\t";
+		cout << data << endl;
+	}
+	~TestDnl()
+	{
+
+	}
+};
+
+int main()
+{
+	TestDnl dataTest;
+	dataTest.cinData();
+	dataTest.printData();
+
+
+	return 0;
+}
+
+
 
